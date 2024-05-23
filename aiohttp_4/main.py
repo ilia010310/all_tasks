93c2832c06a7cd5b75bc022728428fda311825c9
Ilia <Theilyaboyarintsev@gmail.com>
2024-05-23 15:27:12 -0300

    create files (for topic - aiohttp_4)

---
+++ aiohttp_4/main.py
@@ -0,0 +1,46 @@
+import time
+import aiohttp
+import asyncio
+from bs4 import BeautifulSoup
+
+
+total = 0
+pages_list = []
+
+
+async def get_length(page: str, session: aiohttp.client.ClientSession, semaphore) -> None:
+    global total
+    async with semaphore, session.head(page) as data:
+        if data.status == 200:
+            content_length = data.headers.get('Content-Length')
+            total += int(content_length)
+            print(content_length)
+        else:
+            print('Error')
+
+async def get_photos(page: str, session: aiohttp.client.ClientSession, semaphore) -> None:
+    global pages_list
+    async with semaphore, session.get(page) as response:
+        result = await response.text()
+        soup = BeautifulSoup(result, 'html.parser')
+        main_tag = soup.find('main')
+
+        img_tags = main_tag.find_all('img')
+        for img in img_tags:
+            one_photo = page[:-10] + '/' + img.get('src')
+            pages_list.append(one_photo)
+    await asyncio.gather(*[asyncio.create_task(get_length(page_photo, session, semaphore)) for page_photo in pages_list])
+
+
+async def main():
+    """Запуск функции и замер времени работы программы"""
+    stat = time.monotonic()
+    semaphore = asyncio.Semaphore(10)
+    page = 'https://asyncio.ru/zadachi/4/index.html'
+    async with aiohttp.ClientSession() as session:
+        await asyncio.create_task(get_photos(page, session, semaphore))
+    print(f'Воемя выполнения программы: {time.monotonic() - stat}')
+    print(total)
+
+
+asyncio.run(main())
