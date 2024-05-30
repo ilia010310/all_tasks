dab66e62f435a816216ee2cabb7af70506c5a989
Ilia <Theilyaboyarintsev@gmail.com>
2024-05-30 17:15:26 -0300

    Create saper

---
+++ Saper/saper.py
@@ -0,0 +1,172 @@
+import random
+
+
+class Cell:
+    """Класс для представления клетки игрового поля"""
+
+    def __init__(self, around_mines=0, mine=False):
+        self.around_mines = 0
+        self.mine = False
+        self.fl_open = False  # закрыта/открыта клетка
+
+
+class GamePole:
+    """Класс для управления игровым полем, размером N x N клеток """
+
+    def __init__(self, size_of_pole: int, mins_quantity: int) -> None:
+        self.size_of_pole = size_of_pole
+        self.mins_quantity = mins_quantity
+        self.pole = None
+        self.init()
+
+    @staticmethod
+    def set_mine(cell: Cell) -> None:
+        cell.mine = True
+
+    def init(self):
+        """Инициализация поля с новой расстановкой мин."""
+        mins_quantity = self.mins_quantity
+        self.pole = []
+        # Сначала создаем пустое поле
+        for i in range(self.size_of_pole):
+            row = []
+            for j in range(self.size_of_pole):
+                row.append(Cell())
+            self.pole.append(row)
+        # установка бомб
+        mines = self.mins_quantity
+        while mines:
+            position = random.randrange(0, self.size_of_pole ** 2)
+
+            column = 0
+
+            while position >= self.size_of_pole:
+                column += 1
+                position -= self.size_of_pole
+            if self.pole[column][position].mine:
+                continue
+            else:
+                self.pole[column][position].mine = True
+                mines -= 1
+
+            # расстановка цифр вокруг бомбы
+
+            if column == 0:
+                if position == 0:
+                    self.pole[column][1].around_mines += 1
+                    self.pole[column + 1][1].around_mines += 1
+                    self.pole[column + 1][0].around_mines += 1
+                elif position == self.size_of_pole - 1:
+                    self.pole[column][position - 1].around_mines += 1
+                    self.pole[column + 1][position].around_mines += 1
+                    self.pole[column + 1][position - 1].around_mines += 1
+                else:
+                    self.pole[column][position + 1].around_mines += 1
+                    self.pole[column][position - 1].around_mines += 1
+                    self.pole[column + 1][position].around_mines += 1
+                    self.pole[column + 1][position + 1].around_mines += 1
+                    self.pole[column + 1][position - 1].around_mines += 1
+
+            elif column == self.size_of_pole - 1:
+                if position == 0:
+                    self.pole[column][1].around_mines += 1
+                    self.pole[column - 1][1].around_mines += 1
+                    self.pole[column - 1][0].around_mines += 1
+                elif position == self.size_of_pole - 1:
+                    self.pole[column][position - 1].around_mines += 1
+                    self.pole[column - 1][position].around_mines += 1
+                    self.pole[column - 1][position - 1].around_mines += 1
+                else:
+                    self.pole[column][position + 1].around_mines += 1
+                    self.pole[column][position - 1].around_mines += 1
+                    self.pole[column - 1][position].around_mines += 1
+                    self.pole[column - 1][position + 1].around_mines += 1
+                    self.pole[column - 1][position - 1].around_mines += 1
+            else:
+                if position == 0:
+                    self.pole[column][1].around_mines += 1
+                    self.pole[column + 1][1].around_mines += 1
+                    self.pole[column - 1][1].around_mines += 1
+                    self.pole[column - 1][0].around_mines += 1
+                    self.pole[column + 1][0].around_mines += 1
+                elif position == self.size_of_pole - 1:
+                    self.pole[column][position - 1].around_mines += 1
+                    self.pole[column - 1][position].around_mines += 1
+                    self.pole[column - 1][position - 1].around_mines += 1
+                    self.pole[column + 1][position].around_mines += 1
+                    self.pole[column + 1][position - 1].around_mines += 1
+
+                else:
+                    self.pole[column][position + 1].around_mines += 1
+                    self.pole[column][position - 1].around_mines += 1
+                    self.pole[column - 1][position].around_mines += 1
+                    self.pole[column - 1][position + 1].around_mines += 1
+                    self.pole[column - 1][position - 1].around_mines += 1
+                    self.pole[column + 1][position + 1].around_mines += 1
+                    self.pole[column + 1][position - 1].around_mines += 1
+                    self.pole[column + 1][position].around_mines += 1
+
+    def show(self) -> None:
+        """Отображение поля в консоли. Если клетка не открыта, то отображается символ #;
+        Мина отображается символом *;
+        Между клетками - пробел."""
+        for i in self.pole:
+            for j in i:
+                if j.fl_open == False:
+                    print('#', end=' ')
+                elif j.mine:
+                    print('*', end=' ')
+                else:
+                    print(j.around_mines, end=' ')
+            # смена строки
+            print()
+
+    def open_cell(self, row: int, column: int) -> bool:
+        self.pole[row - 1][column - 1].fl_open = True
+        if self.pole[row - 1][column - 1].mine:
+            return False
+        else:
+            return True
+
+    def open_mine(self, row: int, column: int) -> bool:
+        self.pole[row - 1][column - 1].fl_open = True
+        if self.pole[row - 1][column - 1].mine:
+            return True
+        else:
+            return False
+    def play(self):
+        print()
+        self.show()
+        print()
+        print("Вы хотите попробовать открыть клеточку(введите 1)?\n"
+              "Хотите обнаружить бомбу(введите 2)?")
+        num = int(input())
+        if num == 1:
+            print('Введите число строчки и столбика через пробел', end=' ')
+            row, column = [int(i) for i in input().split()]
+            result = self.open_cell(row, column)
+            if result:
+                print()
+                self.play()
+            else:
+                print('Вы проирали')
+        elif num == 2:
+            print('Введите число строчки и столбика через пробел', end=' ')
+            row, column = [int(i) for i in input().split()]
+            result = self.open_mine(row, column)
+            if result:
+                print()
+                self.play()
+            else:
+                print('Вы проирали')
+        else:
+            print('Вы ввели некоректное значение')
+            self.play()
+
+
+
+n = 10  # размер поля
+m = 12  # количество мин
+pole_game = GamePole(n, m)
+pole_game.play()
+
